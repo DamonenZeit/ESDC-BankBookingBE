c01cf112e628516070b0f7681a10786d0d671bac
DamonenZeit <vmh25799@gmail.com>
2023-11-30 13:54:53 +0700

    init project

---
+++ src/main/java/com/esdc/bankBookingBE/BankBookingBeApplication.java
@@ -0,0 +1,13 @@
+package com.esdc.bankBookingBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBookingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBookingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esdc/bankBookingBE/BankBookingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esdc.bankBookingBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBookingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
